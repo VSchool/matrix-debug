90e0383856458183bbf3b67d8db0b1f8e40e8c77
Eric Jonese <ericnatejones@gmail.com>
2017-10-26 11:23:23 -0600

    broken code

---
+++ app.js
@@ -0,0 +1,12 @@
+var matrix = [];
+rows = 3;
+columns = 3;
+
+for (var i = 0; i < rows.length; i++){
+  matrix.push([])
+  for (var j = 0; i < colums.length; i++){
+    matrix[i].push(0)
+  }
+}
+
+console.log(matrix);
